76a77784c331f3deae894e18da24c24e29759121
o-noji <okaden.denden@gmail.com>
2022-10-24 01:46:24 +0900

    🐛

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home({ posts }) {
                 <h3 className={styles.postTitle}>
                   <Link href={`/${post.id}`}>
                     <a>
-                      <Text text={post.properties.Title.id} />
+                      <Text text={post.properties.Name.title} />
                     </a>
                   </Link>
                 </h3>
